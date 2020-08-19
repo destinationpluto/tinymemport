996d0248435d0c42a65af224359b69d1859a9418
Oliver Altergott <altergott.oliver@gmail.com>
2020-08-19 21:44:22 +0200

    added memory buffer as parameter

--- include/memport.h
+++ include/memport.h
@@ -11,15 +11,17 @@ namespace tinymemport
 		static_assert(std::is_default_constructible<T>::value, "DataType must be default-constructible");
 		static_assert(std::is_assignable<T, T>::value, "DataType must be a assignable");
 
-		TDataPort() : m_guard(), m_data_p()
+		TDataPort(uint16_t f_bufferBytes) : m_guard(), m_data_p()
 		{
 			std::lock_guard<std::mutex> lock(m_guard);
-			m_data_p = new (std::nothrow) T;
+			m_rawMem_p = new (std::nothrow) char[sizeof(T)+f_bufferBytes];
+			m_data_p = reinterpret_cast<T *>(m_rawMem_p);
 		}
 		~TDataPort()
 		{
 			std::lock_guard<std::mutex> lock(m_guard);
-			delete m_data_p;
+			delete[] m_rawMem_p;
+			m_data_p = nullptr;
 		}
 
 		void setData(const T f_outputData)
@@ -45,6 +47,7 @@ namespace tinymemport
 
 	private:
 		std::mutex m_guard;
+		char* m_rawMem_p;
 		T *m_data_p;
 	};
 } // namespace tinymemport
